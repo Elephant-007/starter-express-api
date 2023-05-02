25d30f9a386fdfbf1ff87564444fe43f7f3090d4
FoReVeR-GH <dream.high.dev@gmail.com>
2023-05-02 09:09:51 -0700

    db

--- app/config/index.js
+++ app/config/index.js
@@ -1,8 +1,9 @@
 const config = {
   port: 5000,
   // db: "mongodb://127.0.0.1:27017/manage",
-  db: "mongodb+srv://taylor:Superstar1217@cluster0.bm3czid.mongodb.net/manage",
   // db: "mongodb+srv://0xgolden23:4C6dGPFvdLXX5FHq@cluster0.bqyctoq.mongodb.net/contact",
+  // db: "mongodb+srv://taylor:Superstar1217@cluster0.bm3czid.mongodb.net/manage",
+  db: "mongodb+srv://smart:0B6KxXpm8XoMun8z@cluster0.8msgkg5.mongodb.net/manage",
   privateKey: "ktj",
 };
 module.exports = config;
