628e255914342e60c565e98eee111133fbc02a17
FoReVeR-GH <dream.high.dev@gmail.com>
2023-05-02 07:15:03 -0700

    middleware

--- app/routes/index.js
+++ app/routes/index.js
@@ -19,43 +19,83 @@ router.put("/api/auth/:id", controller.api.auth.update);
 router.delete("/api/auth/:id", controller.api.auth.delete);
 //--------------middleware here--------------------
 //-----------api here
-router.use("/", authMiddleware);
-router.get("/api/profile", controller.api.profile.readProfile);
-router.post("/api/profile", controller.api.profile.createProfile);
-router.put("/api/profile/:id", controller.api.profile.updateProfile);
-router.delete("/api/profile/:id", controller.api.profile.deleteProfile);
+router.get("/api/profile", authMiddleware, controller.api.profile.readProfile);
+router.post(
+  "/api/profile",
+  authMiddleware,
+  controller.api.profile.createProfile
+);
+router.put(
+  "/api/profile/:id",
+  authMiddleware,
+  controller.api.profile.updateProfile
+);
+router.delete(
+  "/api/profile/:id",
+  authMiddleware,
+  controller.api.profile.deleteProfile
+);
 
-router.get("/api/experience", controller.api.experience.readExperience);
-router.post("/api/experience", controller.api.experience.createExperience);
-router.put("/api/experience/:id", controller.api.experience.updateExperience);
+router.get(
+  "/api/experience",
+  authMiddleware,
+  controller.api.experience.readExperience
+);
+router.post(
+  "/api/experience",
+  authMiddleware,
+  controller.api.experience.createExperience
+);
+router.put(
+  "/api/experience/:id",
+  authMiddleware,
+  controller.api.experience.updateExperience
+);
 router.delete(
   "/api/experience/:id",
+  authMiddleware,
   controller.api.experience.deleteExperience
 );
 
-router.get("/api/parent", controller.api.parent.read);
-router.post("/api/parent", controller.api.parent.create);
-router.put("/api/parent/:id", controller.api.parent.update);
-router.delete("/api/parent/:id", controller.api.parent.delete);
+router.get("/api/parent", authMiddleware, controller.api.parent.read);
+router.post("/api/parent", authMiddleware, controller.api.parent.create);
+router.put("/api/parent/:id", authMiddleware, controller.api.parent.update);
+router.delete("/api/parent/:id", authMiddleware, controller.api.parent.delete);
 
-router.get("/api/interview", controller.api.interview.read);
-router.post("/api/interview", controller.api.interview.create);
-router.put("/api/interview/:id", controller.api.interview.update);
-router.delete("/api/interview/:id", controller.api.interview.delete);
+router.get("/api/interview", authMiddleware, controller.api.interview.read);
+router.post("/api/interview", authMiddleware, controller.api.interview.create);
+router.put(
+  "/api/interview/:id",
+  authMiddleware,
+  controller.api.interview.update
+);
+router.delete(
+  "/api/interview/:id",
+  authMiddleware,
+  controller.api.interview.delete
+);
 
-router.get("/api/asset", controller.api.asset.read);
-router.post("/api/asset", controller.api.asset.create);
-router.put("/api/asset/:id", controller.api.asset.update);
-router.delete("/api/asset/:id", controller.api.asset.delete);
+router.get("/api/asset", authMiddleware, controller.api.asset.read);
+router.post("/api/asset", authMiddleware, controller.api.asset.create);
+router.put("/api/asset/:id", authMiddleware, controller.api.asset.update);
+router.delete("/api/asset/:id", authMiddleware, controller.api.asset.delete);
 
-router.get("/api/project", controller.api.project.read);
-router.post("/api/project", controller.api.project.create);
-router.put("/api/project/:id", controller.api.project.update);
-router.delete("/api/project/:id", controller.api.project.delete);
+router.get("/api/project", authMiddleware, controller.api.project.read);
+router.post("/api/project", authMiddleware, controller.api.project.create);
+router.put("/api/project/:id", authMiddleware, controller.api.project.update);
+router.delete(
+  "/api/project/:id",
+  authMiddleware,
+  controller.api.project.delete
+);
 
-router.get("/api/address", controller.api.address.read);
-router.post("/api/address", controller.api.address.create);
-router.put("/api/address/:id", controller.api.address.update);
-router.delete("/api/address/:id", controller.api.address.delete);
+router.get("/api/address", authMiddleware, controller.api.address.read);
+router.post("/api/address", authMiddleware, controller.api.address.create);
+router.put("/api/address/:id", authMiddleware, controller.api.address.update);
+router.delete(
+  "/api/address/:id",
+  authMiddleware,
+  controller.api.address.delete
+);
 
 module.exports = router;
